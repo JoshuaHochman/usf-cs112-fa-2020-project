be06fc1eeb8fe27acdec88f6e33a7c2b3f2922ce
Joshua Hochman <jlhochman@dons.usfca.edu>
2020-10-19 21:04:53 -1000

    updated main java class

--- MLLibrary/Main.java
+++ MLLibrary/Main.java
@@ -1,5 +1,19 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello from REPO 2");
+		ArrayList<DataPoint> trainData = new ArrayList<DataPoint>();
+		trainData.add(new DataPoint(1, 2, "Japanese", "Action"));
+		trainData.add(new DataPoint(2, 1, "American", "Strategy"));
+		trainData.add(new DataPoint(3, 5, "Japanese", "Strategy"));
+		trainData.add(new DataPoint(2, 6, "American", "Action"));
+		trainData.add(new DataPoint(3, 7, "American", "Action"));
+		trainData.add(new DataPoint(4, 8, "Japanese", "Strategy"));
+		System.out.println(trainData.get(1).toString());
+
+		ArrayList<DataPoint> data = new ArrayList<DataPoint>();
+		data.add(new DataPoint(double Random, double Random, "", ""))
+
 	}
 }
